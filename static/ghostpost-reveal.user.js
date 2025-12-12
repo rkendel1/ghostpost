8926ce8af4050f05c7917823b5d578c4863d7651
rkendel1 <148293244+rkendel1@users.noreply.github.com>
2025-12-12 15:33:57 -0500

    Bump version to 2.4.4

--- static/ghostpost-reveal.user.js
+++ static/ghostpost-reveal.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ghostpost Reveal
 // @namespace    https://ghostpost-six.vercel.app
-// @version      2.4.3
+// @version      2.4.4
 // @description  Reveal hidden Ghostpost messages on any webpage with one click - now with inline decoding and countdown!
 // @author       Ghostpost
 // @match        *://*/*
